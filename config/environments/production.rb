c83033b5793a441fe9232fb30d773c1aa720f9be
tuantranpham204 <12a1tranphamtuan@gmail.com>
2025-12-16 15:01:52 +0700

    Use SSL and the Puma web server in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 end
